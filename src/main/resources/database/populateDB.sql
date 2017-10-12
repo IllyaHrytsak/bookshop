28deeeed3e4c1e08f76a95c1c56ef07164d9aeb9
IllyaHrytsak <gritsak_web@bigmir.net>
2017-10-12 18:41:22 +0300

    populateDB.sql was changed

--- src/main/resources/database/populateDB.sql
+++ src/main/resources/database/populateDB.sql
@@ -1,11 +1,19 @@
 INSERT INTO role (ROLE_NAME) VALUES ('ADMIN'), ('USER'), ('BLOCKED');
 INSERT INTO account (EMAIL, PASSWORD, FIRST_NAME, LAST_NAME, PHONE_NUMBER, ROLE_ID)
-VALUES ('123',
-        '$2a$04$8X/kPX4FCQ.LGG4.6O2IfuJ6FHIbh7Sad1JxOuckT5GkmLP8hK9a2',
+VALUES ('admin',
+        '$2a$04$VFj2kmDuiH3vA8wEmwImS.elOaZDEZQ0Xjx./HTR0YN9Wgf27iFjm',
         'Illya',
         'Hrytsak',
         '+380935215824',
-        1);
+        1),
+        ('user',
+        '$2a$04$vZoZ/unmpGFoJnVEtfLX1efU16IeanmJ9vzf0YJtXvZAhhzrmDnue',
+        'Igor',
+        'Petrov',
+        '+380673890102',
+        2);
 INSERT INTO book (BOOK_TITLE, BOOK_AUTHOR, BOOK_PRICE) VALUES
-        ('Java', 'Illya Hrytsak', 100),
-        ('C++', 'Dima Andreev', 200);
+        ('Java Core', 'Vlad Borisov', 100),
+        ('C++ Core', 'Dima Andreev', 200),
+        ('MySql Core', 'Vadim Baranin', 265),
+        ('Spring Core', 'Ivan Pupkin', 399);
